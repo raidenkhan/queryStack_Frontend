74970a07639037dbd51e7ea2f3db0bb14ff3cfba
iamemmanuel <96030339+raidenkhan@users.noreply.github.com>
2023-10-06 15:35:44 +0000

    dded features

--- jsfiles/script.js
+++ jsfiles/script.js
@@ -1,1 +1,6 @@
-console.log("hello");
+//nav button
+const navBtn=document.querySelector('button.navbar-toggler');
+const sideContainer=document.querySelector(".sidebar__container");
+navBtn.addEventListener('click',()=>{
+  sideContainer.classList.toggle('show');
+})
